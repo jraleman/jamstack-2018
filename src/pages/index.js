03c4f81fa2c20c09adce1796788954c8746a5c3c
its-gio <me@its-g.io>
2018-11-03 16:53:50 -0700

    Added date picker

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
+import DayPicker from 'react-day-picker'
+
+import 'react-day-picker/lib/style.css'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <DayPicker selectedDays={new Date()} />
+    <DayPicker selectedDays={new Date()} />
   </Layout>
 )
 
