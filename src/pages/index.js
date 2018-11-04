63082683f89002e4eb60444f3ea877a6e117e59b
mokerlund <okerlund.megan@gmail.com>
2018-11-04 11:25:01 -0800

    Centered header and added Sparklines with minimal styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,22 @@ import DayPicker from 'react-day-picker'
 
 import 'react-day-picker/lib/style.css'
 
+import { Sparklines, SparklinesLine, SparklinesSpots } from 'react-sparklines'
+
 const IndexPage = () => (
   <Layout>
     <DayPicker selectedDays={new Date()} />
     <DayPicker selectedDays={new Date()} />
+    <div>
+      <Sparklines data={[5, 10, 5, 20, 8, 15, 25, 7]} limit={8} width={70} height={18} margin={5}>
+        <SparklinesLine style={{ fillOpacity: ".5", fill: "black", r: "3" }} />
+        <SparklinesSpots style={{ fill: "red" }} />
+        <circle fill="black" opacity="100" r="4"></circle>
+      </Sparklines>
+    </div>
   </Layout>
 )
+
+
 
 export default IndexPage
