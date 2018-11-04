bc20e3310b4b77bb25845eb4a96d74e15a8ebbad
mokerlund <okerlund.megan@gmail.com>
2018-11-04 15:03:07 -0800

    Added data placeholders

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,11 +12,23 @@ const IndexPage = () => (
     <DayPicker selectedDays={new Date()} />
     <DayPicker selectedDays={new Date()} />
     <div>
+      <h2>Week Overview</h2> 
+      {/* <P>DAY (import)</P> */}
       <Sparklines data={[5, 10, 5, 20, 8, 15, 25, 7]} limit={8} width={70} height={18} margin={5}>
-        <SparklinesLine style={{ fillOpacity: ".5", fill: "black", r: "3" }} />
+        <SparklinesLine style={{ fillOpacity: ".5", fill: "black", r: "1" }} />
         <SparklinesSpots style={{ fill: "red" }} />
-        <circle fill="black" opacity="100" r="4"></circle>
+        {/* <circle fill="black" opacity="100" r="1"></circle> */}
       </Sparklines>
+    </div>
+    <div>
+      <h2 style={{ display: 'inline' }}>Date: </h2>
+      <span> {"{ dateData }"} </span><br></br>
+      <h2 style={{ display: 'inline' }}>Meal Type: </h2>
+      <span> {"{ mealTypeData }"} </span><br></br>
+      <h2 style={{ display: 'inline' }}>Meal Name: </h2>
+      <span> {"{ mealNameData }"} </span><br></br>
+      <h2 style={{ display: 'inline' }}>Meal Data: </h2>
+      <span> {"{ mealData }"} </span><br></br>
     </div>
   </Layout>
 )
